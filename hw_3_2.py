65c9cbe2e5825913e16578ca503e130983d83312
YuliaKyrylenko <yulkirilenko@gmail.com>
2025-12-06 16:53:40 +0200

    added file hw_3_2

---
+++ hw_3_2.py
@@ -0,0 +1,17 @@
+list_1 = [12, 3, 4, 10]
+list_2 = [1]
+list_3 = []
+list_4 = [12, 3, 4, 10, 8]
+
+list_1 = list_1[-1:] + list_1[:-1]
+list_2 = list_2[-1:] + list_2[:-1]
+list_3 = list_3[-1:] + list_3[:-1]
+list_4 = list_4[-1:] + list_4[:-1]
+print(list_1)
+print(list_2)
+print(list_3)
+print(list_4)
+
+
+
+
