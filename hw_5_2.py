e11438e085e9fbfaed69beeed86409e195ed83d0
YuliaKyrylenko <yulkirilenko@gmail.com>
2025-12-15 13:29:40 +0200

    added file hw_5_2

---
+++ hw_5_2.py
@@ -0,0 +1,24 @@
+while True:
+    num_1 = float(input("Enter a first number: "))
+    num_2 = float(input("Enter a second number: "))
+    oper = input("Enter a operator ( +, -, *, / ): ")
+
+    if oper == "+":
+        result = num_1 + num_2
+    elif oper == "-":
+        result = num_1 - num_2
+    elif oper == "*":
+        result = num_1 * num_2
+    elif oper == "/":
+        if num_2 > 0:
+            result = num_1 / num_2
+        else :
+            result = "Division by zero is not possible"
+    else:
+        result = "You have entered an invalid operator"
+
+    print(result)
+
+    quest = input("Do you want to continue (yes / no): ").lower()
+    if quest != "yes":
+        break
