e63f2af81ed39d60d3ef600b6df42ee5f7bf22bb
YuliaKyrylenko <yulkirilenko@gmail.com>
2026-01-21 11:56:46 +0200

    added files hw_10_1 and hw_10_2

---
+++ hw_10_2.py
@@ -0,0 +1,20 @@
+def first_word(text: str) -> str:
+    """ Пошук першого слова """
+    from string import punctuation
+    text = text.lstrip(punctuation + " ")
+    word = ""
+    for letter in text:
+        if letter.isalpha() or letter == "'":
+            word += letter
+        else:
+            if word:
+                break
+    return word
+
+assert first_word("Hello world") == "Hello", 'Test1'
+assert first_word("greetings, friends") == "greetings", 'Test2'
+assert first_word("don't touch it") == "don't", 'Test3'
+assert first_word(".., and so on ...") == "and", 'Test4'
+assert first_word("hi") == "hi", 'Test5'
+assert first_word("Hello.World") == "Hello", 'Test6'
+print('OK')
