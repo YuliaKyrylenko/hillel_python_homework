bb6c01840daccaf666131e48ad21a7e996af6d8e
YuliaKyrylenko <yulkirilenko@gmail.com>
2026-01-28 12:44:45 +0200

    changed file for test

--- hw_12_1.py
+++ hw_12_1.py
@@ -1,14 +1,20 @@
 import codecs
 import re
 
-def delete_html_tags(html_file, result_file='cleaned.txt'):
-    with open(html_file, 'r', encoding='utf-8') as file:
-        html = file.read()
-    clean_text = re.sub(r'<.*?>', '', html)
-    print(clean_text)
-    with open(result_file, 'w', encoding='utf-8') as new_file:
-        new_file.write(clean_text)
-print(delete_html_tags('draft.html', 'cleaned.txt'))
+# def delete_html_tags(html_file, result_file='cleaned.txt'):
+#     with open(html_file, 'r', encoding='utf-8') as file:
+#         html = file.read()
+
+
+    # clean_text = html #re.sub(r'<.*?>', '', html)
+    # with open(result_file, 'w', encoding='utf-8') as new_file:
+    #     new_file.write(clean_text)
+#     return clean_text
+# print(delete_html_tags('draft.html', 'cleaned.txt'))
+
+with open('cleaned.txt','r') as f:
+    print(f.readline())
+
 
 
 
