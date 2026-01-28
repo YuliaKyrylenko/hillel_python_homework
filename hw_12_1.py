ecfc987b0e3f9d47bdfa6d1139af6bc7dc4c8879
YuliaKyrylenko <yulkirilenko@gmail.com>
2026-01-28 11:37:54 +0200

    added file cleaned.txt

--- hw_12_1.py
+++ hw_12_1.py
@@ -1,4 +1,15 @@
 import codecs
+import re
+
 def delete_html_tags(html_file, result_file='cleaned.txt'):
-      with codecs.open(html_file, 'r', 'utf-8') as file:
-           html = file.read()
+    with open(html_file, 'r', encoding='utf-8') as file:
+        html = file.read()
+    clean_text = re.sub(r'<.*?>', '', html)
+    print(clean_text)
+    with open(result_file, 'w', encoding='utf-8') as new_file:
+        new_file.write(clean_text)
+print(delete_html_tags('draft.html', 'cleaned.txt'))
+
+
+
+
